9a988cc11c2073b8a0428092d77c491d6428124c
terao_tomohiro_44 <zio.tt.dev@gmail.com>
2023-08-15 20:42:22 +0900

    first_commit

---
+++ lib/arr2hash.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "arr2hash/version"
+
+module Arr2hash
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/arr2hash/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Arr2hash
+  VERSION = "0.1.0"
+end
